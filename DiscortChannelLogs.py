bffaf7d0c0db875606f364c963da05a8f78b740f
Jakub Gołaszewski <qbagol777@gmail.com>
2022-02-10 19:32:03 +0100

    Add files via upload

---
+++ DiscortChannelLogs.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+import requests
+import json
+import pandas
+
+def Discord_bot_get():
+    
+    channel_id='<>'
+    key='<>'
+    
+    now = datetime.now()
+    time = now.strftime("%H.%M.%S")
+   
+    url=f'https://discord.com/api/v9/channels/{channel_id}/messages'
+
+    header = {
+        'authorization' : f'{key}'
+    }
+
+    get_response=requests.get(url, headers=header)
+    load_response=json.loads(get_response.text)
+    df = pandas.DataFrame(columns=['user','message','time'])
+    
+    for i in range(len(load_response)):
+        df = df.append({'user':load_response[i]['author']['username'], 'message':load_response[i]['content'], 'time':load_response[i]['timestamp']}, ignore_index=True)
+    
+    df.to_csv(f'discord_logs_channel_{channel_id}_time_{time}.csv')
+    
+x=Discord_bot_get()
